02a0611e0c7f3226881245375332f84a68cf0a52
Jeevanshetty1999 <jeevanshetty1999@gmail.com>
2021-06-15 11:44:48 +0530

    view
    view

--- screens/CollegeScreen.js
+++ screens/CollegeScreen.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import CollegeData from '../dbscreens/CollegeData';
+//import React from 'react';
+//import CollegeData from '../dbscreens/CollegeData';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 const CollegeScreen = ( { navigation } ) => {
-    return (
-      <CollegeData/>
-    );
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://192.168.76.93:8000/api/Collegesshow/')
+      .then((response) => response.json())
+      .then((json) => { setData(json)})
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 150 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.c_name}, {item.caddress}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
   }
 
   export default CollegeScreen;
--- screens/CompanyScreen.js
+++ screens/CompanyScreen.js
@@ -1,9 +1,31 @@
-import React from 'react';
 import CompanyData from '../databasescreens/CompanyData';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 const CompanyScreen = ( { navigation } ) => {
-    return (
-      <CompanyData/>
-    );
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://192.168.76.93:8000/api/Companiesshow/')
+      .then((response) => response.json())
+      .then((json) => { setData(json)})
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 150 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.comp_name}, {item.caddress}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
   }
   export default CompanyScreen;
